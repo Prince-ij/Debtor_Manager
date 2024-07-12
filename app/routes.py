56ce60485d7be209d63a94f91450ba8be3899735
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-07-12 17:15:46 +0100

    implemented login template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template, flash, redirect
+from flask import url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Successful')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
