e81bf78a85aa35773101fa8be82065c6259ec7af
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-07-12 23:48:32 +0100

    added templates

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), nullable=False)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), nullable=False, unique=True)
+    total_debtors: so.Mapped[int] = so.mapped_column(sa.Integer, default=0)
+    address: so.Mapped[str] = so.mapped_column(sa.String(255))
+    phone_number: so.Mapped[str] = so.mapped_column(sa.String(15))
+    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
+    total_amount_due: so.Mapped[float] = so.mapped_column(sa.Float, default=0.0)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+class Debtor(db.Model):
+    __tablename__ = 'debtors'
+    
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), nullable=False)
+    amount_due: so.Mapped[float] = so.mapped_column(sa.Float, nullable=False)
+    description: so.Mapped[str] = so.mapped_column(sa.String(255))
+
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('users.id'), nullable=False)
+    user: so.Mapped[User] = so.relationship('User', back_populates='debtors')
+
+    def __repr__(self):
+        return f'<Debtor {self.name}>'
+
+User.debtors = so.relationship('Debtor', order_by=Debtor.id, back_populates='user')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
-from app import app
-from flask import render_template, flash, redirect
-from flask import url_for
+from app import app, db
+import sqlalchemy as sa
+from flask_login import login_required
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User, Debtor
 
 @app.route('/')
 @app.route('/index')
@@ -10,8 +13,50 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login Successful')
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+                sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@login_required
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/add_debtor')
+def add_debtor():
+    return render_template('add_debtor.html')
+
+@app.route('/remove')
+def remove():
+    return redirect(url_for('view_debtor'))
+
+@app.route('/view_debtor')
+def view_debtor():
+    return render_template('view_debtor.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
